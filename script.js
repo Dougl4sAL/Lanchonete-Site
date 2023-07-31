c5f9b9f0a3b6b93b42619b3fd576ba0e7d605791
Douglas Almeida <dougsouza1708@gmail.com>
2023-07-31 15:26:02 -0300

    js iniciacao

---
+++ script.js
@@ -0,0 +1,34 @@
+let mais_produto = document.querySelectorAll('.mais-produto')
+let menos_produto = document.querySelectorAll('.menos-produto')
+let quantidades = Array(mais_produto.length).fill(0); // inicializa o array com zeros
+// Cria um array(lista) contendo o valor da quantidade de cada card separado
+let carrinho = document.querySelectorAll('.carrinho')
+
+// selectorAll cria uma lista com todos os nomes, por isso precisa usar o forEach
+// pra varrer todos e saber qual foi clicado passando o index dele
+mais_produto.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        let quantidade = document.querySelectorAll('.quantidade')[index]
+        quantidades[index] += 1
+        quantidade.innerHTML = quantidades[index]
+    })
+})
+
+menos_produto.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        let quantidade = document.querySelectorAll('.quantidade')[index]
+        if (quantidades[index] > 0) {
+            quantidades[index] -= 1
+            quantidade.innerHTML = quantidades[index]
+        }
+    })
+})
+
+carrinho.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        let quantidade = document.querySelectorAll('.quantidade')[index]
+
+        quantidades[index] = 0
+        quantidade.innerHTML = quantidades[index]
+    })
+})
