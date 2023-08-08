3c61a9ba881a54690d4a2040b6eb20e24cfb1dad
Douglas Almeida <dougsouza1708@gmail.com>
2023-08-08 12:24:00 -0300

    commit

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let mais_produto = document.querySelectorAll('.mais-produto')
 let menos_produto = document.querySelectorAll('.menos-produto')
 let quantidades = Array(mais_produto.length).fill(0); // inicializa o array com zeros
+let valores_produtos = document.querySelectorAll('.valor-produto')
 
 // Cria um array(lista) contendo o valor da quantidade de cada card separado
 let add_carrinho = document.querySelectorAll('.add-carrinho')
+
+let valor_total = document.querySelector('.valor-total')
+let total_itens = document.querySelector('.total-itens')
+
+valor_total.addEventListener('click', add_valor_total_carrinho)
+
+function add_valor_total_carrinho(quantidade, valor) {
+    console.log(`adicionou x${quantidade} no valor de ${valor}`);
+    let valorAtual = parseFloat(valor_total.innerText.replace('R$', ''));
+    let itensAtuais = parseInt(total_itens.innerText);
+
+    valor_total.innerText = `R$ ${(valorAtual + quantidade * valor).toFixed(2)}`;
+    total_itens.innerText = `${itensAtuais + quantidade} itens`;
+
+}
 
 // selectorAll cria uma lista com todos os nomes, por isso precisa usar o forEach
 // pra varrer todos e saber qual foi clicado passando o index dele
@@ -27,9 +43,12 @@ menos_produto.forEach((btn, index) => {
 
 add_carrinho.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        let quantidade = document.querySelectorAll('.quantidade')[index]
+        let quantidade = quantidades[index];  // quantidade do produto
+        let valor = parseFloat(valores_produtos[index].innerText.replace('R$', '')); // valor do produto
 
-        quantidades[index] = 0
-        quantidade.innerHTML = quantidades[index]
-    })
-})
+        add_valor_total_carrinho(quantidade, valor);
+
+        quantidades[index] = 0;
+        document.querySelectorAll('.quantidade')[index].innerText = '0'; // reset the displayed quantity
+    });
+});
